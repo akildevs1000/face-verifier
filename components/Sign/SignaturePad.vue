b45d1951defaf957f213fb9ea5c0248b374217e8
akildevs1000 <akildevs1000@gmail.com>
2024-12-18 12:44:25 +0400

    ui changes

--- components/Sign/SignaturePad.vue
+++ components/Sign/SignaturePad.vue
@@ -1,37 +1,16 @@
 <template>
-  <div
-    style="
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      gap: 20px;
-    "
-    class="pa-5"
-  >
-    <div
-      class="text-h6"
-      style="
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 10px;
-      "
-    >
-      Sign Here
-      <VueSignaturePad
-        style="border: 1px solid #6946dd; flex: 1; width: calc(80vw - 15px)"
-        ref="signaturePad"
-        height="550px"
-      />
+  <div class="text-center d-flex">
+    <div id="video-container">
+      <VueSignaturePad ref="signaturePad" />
     </div>
-
     <div
+      class="camera-btn-container"
       style="
-        display: flex;
-        align-items: center;
-        justify-content: center;
         flex-direction: column;
         gap: 10px;
+        width: 25%;
+        height: 100vh;
+        background: rgb(224, 224, 224);
       "
     >
       <v-avatar
